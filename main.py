c1c1600a3b4c251aaab50d8da20da189d178b47a
rmiller003 <rmiller003@gmail.com>
2020-10-30 04:46:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Welcome to Adventure X!")
+name = input("What is your name? ")
+age = input("What is your age ")
+
+print("Hello", name, "you are", age, "years old.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Data Types:
+string "hello", 'hi, "89"
+int 8, 7, -9, 10000
+float 6.0, 7.5, -9.8, -100.0
+bool True, False
+'''
